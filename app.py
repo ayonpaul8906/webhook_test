a6dea7e19f6d5d40a0617c7647b1331a1210647b
Ayon Paul <ayonpaul8906@gmail.com>
2026-04-11 12:09:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+OPENAI_API_KEY= process.env.OPENAI_API_KEY_0
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True, use_reloader=False)
